f6eee8856813a951f8c03b9c42cc2379ce5a5ab6
ANTHONY MALKOUN <antounmalkoun@gmail.com>
2019-11-09 20:56:23 +0200

    Put pins at starting position. Added the logic for moving the pins: picking the die, calculating where the pins should be and moving them accordingly.

--- snakesandladders.cpp
+++ snakesandladders.cpp
@@ -5,6 +5,12 @@
 
 snakesandladders::snakesandladders(User *user, int gID)
 {
+    pin1_x = 0;
+    pin1_y = 0;
+    pin2_x = 0;
+    pin2_y = 0;
+    player_rolled = false;
+    player_picked = false;
     GridL = new QGridLayout;
     PushButton_Exit = new QPushButton("Exit");
     PushButton_Roll = new QPushButton("Roll Dice");
@@ -13,18 +19,24 @@ snakesandladders::snakesandladders(User *user, int gID)
     PushButton_Die1->hide();
     PushButton_Die2->hide();
 
-    //board goes from x:200 y:0 to x:980 y:550 so each square is 78x55
+    // OLD: board goes from x:200 y:0 to x:980 y:550 so each square is 78x55
+    // NEW: each square is 65x65
     board = new QGraphicsPixmapItem();
     board->setPixmap((QPixmap("/home/eece435l/project_ja_9/images/background2.png")));
     board->setPos(200,0);
 
     player1_pin = new QGraphicsPixmapItem();
     player1_pin->setPixmap((QPixmap("/home/eece435l/project_ja_9/images/pin1.png")).scaled(40,40));
-    player1_pin->setPos(980,0);
+//    player1_pin->setPos(980,0);old
+//    player1_pin->setPos(785,10); new top right
+    player1_pin->setPos(200,595); // new bot left
+//    player1_pin->setPos(200 + 65, 595 - 65);
 
     player2_pin = new QGraphicsPixmapItem();
     player2_pin->setPixmap((QPixmap("/home/eece435l/project_ja_9/images/pin2.png")).scaled(40,40));
-    player2_pin->setPos(200,550);
+//    player2_pin->setPos(200,550);
+    player2_pin->setPos(230,595); // new bot left
+//    player2_pin->setPos(230 + 65*9, 595 - 65*8);
 
     snake1= new snake(20,5);
     snake1->setPixmap((QPixmap("/home/eece435l/project_ja_9/images/snake1.png")).scaled(200,200));
@@ -82,6 +94,8 @@ snakesandladders::snakesandladders(User *user, int gID)
 
     QObject::connect(PushButton_Exit, SIGNAL(clicked(bool)), this, SLOT(exit()));
     QObject::connect(PushButton_Roll, SIGNAL(clicked(bool)), this, SLOT(rolldice()));
+    QObject::connect(PushButton_Die1, SIGNAL(clicked(bool)), this, SLOT(player_picked_die1()));
+    QObject::connect(PushButton_Die2, SIGNAL(clicked(bool)), this, SLOT(player_picked_die2()));
 }
 
 void snakesandladders::exit() {
@@ -91,10 +105,90 @@ void snakesandladders::exit() {
 }
 
 void snakesandladders::rolldice() {
-    int rand1 = rand() % 6 + 1;
-    int rand2 = rand() % 6 + 1;
-    PushButton_Die1->setText(QString::number(rand1));
-    PushButton_Die2->setText(QString::number(rand2));
-    PushButton_Die1->show();
-    PushButton_Die2->show();
+    if (!player_rolled) {
+        int rand1 = rand() % 6 + 1;
+        int rand2 = rand() % 6 + 1;
+        PushButton_Die1->setText(QString::number(rand1));
+        PushButton_Die2->setText(QString::number(rand2));
+        PushButton_Die1->show();
+        PushButton_Die2->show();
+        player_rolled = true;
+        player_picked = false;
+    }
+}
+
+void snakesandladders::player_picked_die1() {
+    if (!player_picked) {
+        qWarning() << PushButton_Die1->text().toInt();
+        move_pins(PushButton_Die1->text().toInt(), PushButton_Die2->text().toInt());
+        player_rolled = false;
+        player_picked = true;
+    }
+}
+
+void snakesandladders::player_picked_die2() {
+    if (!player_picked) {
+        qWarning() << PushButton_Die2->text().toInt();
+        move_pins(PushButton_Die2->text().toInt(), PushButton_Die1->text().toInt());
+        player_rolled = false;
+        player_picked = true;
+    }
+}
+
+void snakesandladders::move_pins(int move1, int move2) {
+    bool pin1_hitTheTop = false;
+    bool pin2_hitTheTop = false;
+
+    for (int i = 0; i < move1; i++) {
+        if (pin1_y == 9 && pin1_x == 0) {
+            pin1_hitTheTop = true;
+        }
+        if (!pin1_hitTheTop) {
+            if (pin1_y % 2 == 0) {
+                if (pin1_x < 9) {
+                    pin1_x++;
+                } else {
+                    pin1_y++;
+                }
+            } else {
+                if (pin1_x > 0) {
+                    pin1_x--;
+                } else {
+                    pin1_y++;
+                }
+            }
+        } else {
+            pin1_x++;
+        }
+    }
+
+    for (int i = 0; i < move2; i++) {
+        if (pin2_y == 9 && pin2_x == 0) {
+            pin2_hitTheTop = true;
+        }
+        if (!pin2_hitTheTop) {
+            if (pin2_y % 2 == 0) {
+                if (pin2_x < 9) {
+                    pin2_x++;
+                } else {
+                    pin2_y++;
+                }
+            } else {
+                if (pin2_x > 0) {
+                    pin2_x--;
+                } else {
+                    pin2_y++;
+                }
+            }
+        } else {
+            pin2_x++;
+        }
+    }
+
+    relocate();
+}
+
+void snakesandladders::relocate() {
+    player1_pin->setPos(200 + 65*pin1_x, 595 - 65*pin1_y);
+    player2_pin->setPos(230 + 65*pin2_x, 595 - 65*pin2_y);
 }
--- snakesandladders.h
+++ snakesandladders.h
@@ -37,10 +37,19 @@ private:
     ladder *ladder3;
     ladder *ladder4;
     int gID;
+    bool player_rolled;
+    bool player_picked;
+    int pin1_x;
+    int pin1_y;
+    int pin2_x;
+    int pin2_y;
 public slots:
     void exit();
     void rolldice();
-
+    void player_picked_die1();
+    void player_picked_die2();
+    void move_pins(int move1, int move2);
+    void relocate();
 };
 
 #endif // SNAKESANDLADDERS_H
